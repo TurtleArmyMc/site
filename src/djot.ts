e7670611794d37ba31e863237e0d9a24635ebc68
TurtleArmyMc <44322335+TurtleArmyMc@users.noreply.github.com>
2024-08-05 16:26:23 -0400

    Open links to external sites in a new tab

--- src/djot.ts
+++ src/djot.ts
@@ -13,6 +13,13 @@ function sidenoteOverrides(): djot.Visitor<djot.HTMLRenderer, string> {
     let sidenoteRefIndex = 0;
 
     const transform = (node: djot.AstNode, context: djot.HTMLRenderer): string | undefined => {
+        if (node.tag === "link") {
+            if (node.destination?.startsWith("http")) {
+                node.attributes ??= {};
+                node.attributes.target = "_blank";
+                node.attributes.rel = "noopener noreferrer";
+            }
+        }
         if (node.attributes && node.attributes["refnote"]) {
             const inx = sidenoteRefIndex++;
             delete node.attributes["refnote"];
